a268b58fa1787c0a18c661fd398fd68a260ed08b
luskinha1212 <luskinha12m@gmail.com>
2026-04-14 09:00:33 -0300

    Update ex01.js

--- ex01.js
+++ ex01.js
@@ -1,3 +1,12 @@
+// Atividade 1
 
+const readline = require('readline-sync');
 
+let nome = readline.question("Digite seu nome: ")
+
+if (nome == "Alice") {
+console.log("Olá, Alice! Bem-vinda de volta!")
+} else {
+console.log(`Olá, ${nome}! Prazer em conhecê-lo!`)
+ }
 
