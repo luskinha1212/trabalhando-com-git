cc62cc9e892e8e1ca6b678e128f2fb36b72fe002
luskinha1212 <luskinha12m@gmail.com>
2026-04-14 09:36:46 -0300

    Update ex10.js

--- ex10.js
+++ ex10.js
@@ -1,9 +1,9 @@
 
 const readline = require('readline-sync')
 
-let totalPaginas = 300
+let totalPaginas = 250
 let paginasLidas = readline.questionInt('Quantas páginas já foram lidas?: ')
-let percentual = (paginasLidas / totalPaginas) * 100
+let percentual = (paginasLidas / totalPaginas) * 50
 
 console.log('\n========== Progresso do Livro ==========' )
 console.log(`Você já concluiu ${Math.round(percentual)}% do livro.`)
