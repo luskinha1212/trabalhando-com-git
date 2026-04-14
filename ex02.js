0c582699171348ba4bd7f73f266d48e9eec394da
luskinha1212 <luskinha12m@gmail.com>
2026-04-14 09:31:26 -0300

    Update ex02.js

--- ex02.js
+++ ex02.js
@@ -1,11 +1,11 @@
-const readline = require('readline-sync');
+const readline = require('readline-sync')
 
-let nome = readline.question("Digite seu nome: ");
-let nomeDoPrato = readline.question("Digite o nome do prato: ");
-let valorDoPrato = readline.question("Digite o valor do prato: R$ ");
+let nome = readline.question("Digite seu nome: ")
+let nomeDoPrato = readline.question("Digite o nome do prato: ")
+let valorDoPrato = readline.question("Digite o valor do prato: R$ ")
 
-console.log(`\n=== Resumo do Pedido ===`);
-console.log(`Cliente: ${nome}`);
-console.log(`Prato escolhido: ${nomeDoPrato}`);
-console.log(`Valor: R$ ${parseFloat(valorDoPrato).toFixed(2)}`);
-console.log(`=====================\n`);
+console.log(`\n=== Resumo do Pedido ===`)
+console.log(`Cliente: ${nome}`)
+console.log(`Prato escolhido: ${nomeDoPrato}`)
+console.log(`Valor: R$ ${parseFloat(valorDoPrato)})
+console.log(`=====================\n`)
